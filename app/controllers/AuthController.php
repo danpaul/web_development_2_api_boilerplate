eb89de6cd903a1be620492ae8b48914cbe9dd918
Dan Breczinski <pt2323@gmail.com>
2025-03-07 16:36:55 +0100

    cleanup

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -38,6 +38,7 @@ class AuthController extends Controller
             $this->userModel->create($data['email'], $data['password']);
             return ResponseService::Send(['message' => 'User registered successfully']);
         } catch (\Exception $e) {
+            var_dump($e);
             ResponseService::Error('Registration failed', 500);
         }
     }
